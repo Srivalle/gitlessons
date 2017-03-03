8f09c5c4d1fd08ff88b7fb905122438398047433
Srivalli Sanipoyina <ssanipoy@cisco.com>
2017-03-03 13:13:31 +0530

    added documentation in the file

--- src/square.py
+++ src/square.py
@@ -1,11 +1,16 @@
 def my_square(x):
 	'''
 	this is sample method that we are editing in GIT Hub and we are going to check in the local git
-	i am adding the line in git hub before pushing file from local to Git hub
+	i am adding the line in git hub before pushing file from local to Git 
+        hub.
+        this fucntion is calculating squares using operator **
 	'''
 	return(x**2)
 
 def add(x,y):
+    '''
+    Adding the document and this function is for addition using the operator +
+    '''
     return (x+y)
 
 my_square(8)
